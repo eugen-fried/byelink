46e49306acfa34c88b8ab7aef7b536874b47bef0
Yevgeniy <evgeny@evgeny-dell.(none)>
2013-03-08 15:31:20 +0200

    First commit

---
+++ buylink-persistence/src/main/java/com/byelink/persistence/dto/TagDto.java
@@ -0,0 +1,33 @@
+package com.byelink.persistence.dto;
+
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: evgeny
+ * Date: 3/8/13
+ * Time: 12:52 PM
+ */
+public class TagDto implements Serializable {
+    private int id;
+    private String value;
+    private String description;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
