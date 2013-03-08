615094ff0a42380cde5ece53e67c0f180cb75715
j-unior <yevg.mord@gmail.com>
2013-03-08 16:04:13 +0200

    Velocity views & google app engine stuff

---
+++ buylink-web/src/main/java/com/byelink/web/controller/SettingsController.java
@@ -0,0 +1,13 @@
+package com.byelink.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: evgeny
+ * Date: 3/8/13
+ * Time: 3:56 PM
+ */
+@Controller
+public class SettingsController {
+}
